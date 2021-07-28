ed531d6fc84d40f348ab1deb84809445577b11bf
Juan Manuel Caffi <juanmanuelcaffi@gmail.com>
2021-07-28 20:44:33 -0300

    Se actualizarón los textos

--- src/components/lottery/ResumeLotteryScreen.js
+++ src/components/lottery/ResumeLotteryScreen.js
@@ -16,8 +16,8 @@ export const ResumeLotteryScreen = () => {
                     <div className="card-body">
                       <h3 className="text-center"> ¿Qué es?  </h3>
                       <span>
-                      Ubi loterry es un sistema creado para el incentivo de la quema de ubis.  
-                      Para esto, se realizan sorteos mensuales, dónde para participar, se exige la quema de mínimo 100 ubis, lo que te brinda 1 ticket para participar. Si deseas "comprar" 2 tickets, deberás quemar 200 ubis, y así sucesivamente. 
+                      Ubi lottery es un sistema creado para el incentivo de la quema de ubis.  
+Para esto, se realizan sorteos mensuales, dónde para participar, se exige la quema de mínimo 100 ubis, lo que te brinda 1 ticket para participar. Si deseas "comprar" 2 tickets, deberás quemar 200 ubis, y así sucesivamente.  
                       </span>
                     </div>
                   </div>
@@ -28,7 +28,7 @@ export const ResumeLotteryScreen = () => {
                   <div className="card-body">
                     <h3 className="text-center"> ¿Cómo funciona? </h3>
                     <span>
-                    Para participar deberas enviar los ubis a una wallet determinada (burn) a la que nadie tiene acceso. Luego, copiar el link de tu wallet con la que realizaste la quema y pegarla en un form de Google, y listo! Ya estás participando!  
+                    Para participar deberás enviar los ubis a una wallet determinada (0x000000000000000000000000000000000000dEaD) a la que nadie tiene acceso, conocida como wallet burn. Luego, copiar la eth wallet (ej.: 0x6986E8C8Da3...) con la que se realizó la transacción y pegarla en un form de Google, y listo! Ya estás participando!   
                     </span>
                   </div>
                   </div>
@@ -41,7 +41,7 @@ export const ResumeLotteryScreen = () => {
                     <div className="card-body">
                       <h3 className="text-center"> ¿Cómo se entregan los premios?  </h3>
                       <span>
-                      La mayoría de los premios van a ser digitales, como subscripciónes a Netflix, amazon prime, HBO, Spotify, etc. El sorteo de realizará en vivo y será mediante un canal de discord. 
+                      La mayoría de los premios van a ser digitales, como suscripciones a Netflix, Amazon prime, HBO, Spotify, etc. El sorteo de realizará en vivo y será mediante un canal de YouTube (https://www.youtube.com/channel/UCqHr6VofPC6oG6cAbE4Q-0g) 
                       </span>
                     </div>
                   </div>
@@ -58,6 +58,19 @@ export const ResumeLotteryScreen = () => {
                   </div>
               </div>
 
+            </div>
+    
+            <div className="mt-4 mb-4 row">
+              <div className="col-xs-12 col-md-12 col-lg-12">
+                <div className="card">
+                    <div className="card-body">
+                      <h3 className="text-center"> ¿Quién puede participar? </h3>
+                      <span>
+                        Todo aquel humano que este registrado en Proof of Humanity. Este es un requisito necesario para validar la identidad a la hora de entregar el premio. (Se hará una videollamada entre el organizador del evento y el ganador).
+                      </span>
+                    </div>
+                  </div>
+              </div>
             </div>
               
 

7c5cb2dd5d8e3fd9bfe55e76a2c1e70c77e271ea
Juan Manuel Caffi <juanmanuelcaffi@gmail.com>
2021-07-31 10:43:54 -0300

    Se agrego el news de Julio

--- src/components/newsletter/newsletters.js
+++ src/components/newsletter/newsletters.js
@@ -1,13 +1,13 @@
 
 export const Newslleters = [
     {
-        id: 1,
-        title : '06-2021',
-        cover: '/newsletters/cover-062021-es.png',
-        link: '/newsletters/062021-es.pdf'
+        id: 2,
+        title : '07-2021',
+        cover: '/newsletters/cover-072021.png',
+        link: '/newsletters/072021.pdf'
     },
     {
-        id: 2,
+        id: 1,
         title : '06-2021',
         cover: '/newsletters/cover-062021-en.png',
         link: '/newsletters/062021-en.pdf'
